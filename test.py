767b94b22e5dfece4fedb279ae37f1a5a59d7ee5
henryjameslau <henry.james.lau@gmail.com>
2023-03-14 21:28:21 +0000

    test

--- test.py
+++ test.py
@@ -3,42 +3,62 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
+import wget
 import json
-# from pyvirtualdisplay import Display
-# display = Display(visible=0, size=(800, 800))  
-# display.start()
+
+# define a function to split a string at a certain occurance of a separator
+
+# https://stackoverflow.com/questions/36300158/split-text-after-the-second-occurrence-of-character
+def split(strng, sep, pos):
+    strng = strng.split(sep)
+    return sep.join(strng[:pos]), sep.join(strng[pos:])
+
 
 chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
                                       # and if it doesn't exist, download it automatically,
                                       # then add chromedriver to path
 
 chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
-options = [
-  # Define window size here
-    #"--window-size=1200,1200",
-    # "--ignore-certificate-errors"
- 
-    "--headless=new",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
-]
+chrome_options.add_argument("--headless=new")
 
-for option in options:
-    chrome_options.add_argument(option)
 
-    
+#open file with last mnt year saved in it
+file = open("mth-yr.txt","r+")
+
 driver = webdriver.Chrome(options = chrome_options)
 
 driver.get('https://corsproxy.io/?https://www.ons.gov.uk/economy/inflationandpriceindices/datasets/consumerpriceindicescpiandretailpricesindexrpiitemindicesandpricequotes/data')
 text= driver.find_element(By.TAG_NAME,'pre').text
 data = json.loads(text)
 datasets = data['datasets']
-with open('./datasets.json', 'w') as f:
-    f.write(json.dumps(datasets))
 
+#go through the dataset and find the first one which doesn't contain the word framework, glossary or /pricequotes. The url includes pricesquotes so that slash is important. Save the index as the variable match  
+for i,dataset in enumerate(datasets):
+    match = i
+    if('framework' not in dataset['uri'] and 'glossary' not in dataset['uri'] and '/pricequotes' not in dataset['uri']):
+        break
+
+#get the uri of the items dataset we want
+items = data['datasets'][match]['uri']
+
+#get the month and year from the uri
+date=split(items,'itemindices',2)[1]
+
+if (file.read() != date):
+    with webdriver.Chrome(options= chrome_options) as driver: 
+        driver.get("https://corsproxy.io/?https://www.ons.gov.uk"+items+"/data")
+
+        itemsurl= driver.find_element(By.TAG_NAME,'pre').text
+        itemspage = json.loads(itemsurl)
+        csv = itemspage['downloads'][0]['file']
+       
+        print("https://corsproxy.io/?https://www.ons.gov.uk/file?uri="+items+"/"+csv)
+#         wget.download("https://corsproxy.io/?https://www.ons.gov.uk/file?uri="+items+"/"+csv)
+        # closing browser
+        driver.close()
+
+
+#     file.write(date)
+
+with open("csv.txt",'w') as file:
+    file.write(csv)
