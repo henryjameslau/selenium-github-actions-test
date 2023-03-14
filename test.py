951c049cf6f12f5b3ebe6c5687ed54490bbf6e83
henryjameslau <henry.james.lau@gmail.com>
2023-03-14 21:41:20 +0000

    no wget

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
-import wget
 import json
 
 # define a function to split a string at a certain occurance of a separator
